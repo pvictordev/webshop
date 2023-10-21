0a6a10c42aa1519b367720dee542d1a256251b6a
Purice Victor <pvictor0110@gmail.com>
2023-10-21 18:43:23 +0300

    changed icon

--- server/index.js
+++ server/index.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
-const ProductModel = require('./models/Products');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const ProductModel = require("./models/Products");
 const app = express();
-app.use(cors());
-app.use(express.json());
+const port = process.env.PORT || 5000;
 
 mongoose.connect("mongodb://localhost:27017/webshopDB");
 
-const port = process.env.PORT || 5000;
+app.use(cors());
 
-app.get('/getProducts', (req, res) => {
-    ProductModel.find()
-    .then(products => res.json(products))
-    .catch(err => res.json(err))
-})
+app.use(express.json());
+
+app.get("/getProducts", (req, res) => {
+  ProductModel.find()
+    .then((products) => res.json(products))
+    .catch((err) => res.json(err));
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
