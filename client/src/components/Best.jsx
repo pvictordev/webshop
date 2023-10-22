9b559085929018a7007ee10326e11ce2c27a83df
Purice Victor <pvictor0110@gmail.com>
2023-10-22 21:34:46 +0300

    added trend and best

--- client/src/components/Best.jsx
+++ client/src/components/Best.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import "../styles/best.scss";
+import Products from "../data/Products";
+import Card from "./Card";
 
 export default function Best() {
+  const best = Products.splice(0, 6).map((product) => {
+    return <Card key={product.id} {...product} />;
+  });
   return (
-    <div>
-      Best products from each category
+    <div className="Best">
+      <div className="Best__header">
+        <h1>Best sellers</h1>
+      </div>
+      <div className="Best__grid">{best}</div>
     </div>
-  )
+  );
 }
--- client/src/components/Trend.jsx
+++ client/src/components/Trend.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import "../styles/trend.scss";
+import Products from "../data/Products";
+import Card from "./Card";
 
 export default function Trend() {
+  const trend = Products.splice(0, 4).map((product) => {
+    return <Card key={product.id} {...product} />;
+  });
   return (
-    <div>
-      Trending products
+    <div className="Trend">
+      <div className="Trend__header">
+        <h1>Trending Products</h1>
+      </div>
+      <div className="Trend__grid">{trend}</div>
     </div>
-  )
+  );
 }
