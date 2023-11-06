cbd56ab6862fc9945db1737a676f34cc4f5ade7a
Purice Victor <pvictor0110@gmail.com>
2023-11-06 17:33:05 +0200

    commit

--- server/index.js
+++ server/index.js
@@ -2,16 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 5000;
-//const Products = require("./Data/Products.js");
-import productsData from "./Data/productsData.js";
+//import productsData from "./productsData.json";
+// Load products
+app.get("/api/productsData", (req, res) => {
+  res.json(productsData);
+});
 
 app.use(cors());
 app.use(express.json());
-
-//load products
-app.get("/api/Products", (req, res) => {
-  res.json(productsData);
-});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
