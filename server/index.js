a20f6a2c65066a7099e9c6b223657e0acae37d26
Purice Victor <pvictor0110@gmail.com>
2023-11-06 15:28:39 +0200

    some server changes

--- server/index.js
+++ server/index.js
@@ -2,9 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 5000;
+//const Products = require("./Data/Products.js");
+import productsData from "./Data/productsData.js";
 
 app.use(cors());
 app.use(express.json());
+
+//load products
+app.get("/api/Products", (req, res) => {
+  res.json(productsData);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
