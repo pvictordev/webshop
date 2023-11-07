707ebb99f0ed4c0488bf5b1206c2b8a327fdfa8d
Purice Victor <pvictor0110@gmail.com>
2023-11-07 09:26:58 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 //const express = require('express');
 import express from "express";
 import productsData from "./Data/productsData.js";
+
 const app = express();
 
-//load productsData
+//load productsData from server
 app.get("/api/productsData", (req, res) => {
+    console.log(productsData)
   res.json(productsData);
+});
+//load product by id
+app.get("/api/productsData/:id", (req, res) => {
+  const product = productsData.find((x) => x.id === req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: "Product not found" });
+  }
 });
 
 app.get("/", (req, res) => {
